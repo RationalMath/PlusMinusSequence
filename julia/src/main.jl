bf81892468be62c167623eb296b49cdb2f28028a
RationalMath <samerweb3.@gmail.com>
2024-11-12 14:04:44 +0100

    4 plots

--- julia/src/main.jl
+++ julia/src/main.jl
@@ -1,48 +1,72 @@
 using Plots
-using Statistics
-using StatsBase
 include("PlusMinusSequence.jl")
 using .PlusMinusSequence
 
-# s100 = generate_plus_minus_sequence(2120)
+num_plots = 4
+plot_size_x = 600
+plot_size_y = 500
 
-# scatter(
-# 	s100,
-# 	marker = :circle,
-# 	title = "Plus-Minus Sequence up to $(length(s3)) values",
-# 	xlabel = "Sequence index",
-# 	ylabel = "Sequence value",
-# 	legend = false,
-# )
+main_title_size = 12
+sub_title_size = 8
 
-s1m = generate_plus_minus_sequence(100000)
-s1m = filter(x -> x % 2 != 0, s1m)
+# Create plot layout with 1 row and 2 columns
+layout = @layout [a; b; c; d]
 
+plot_settings = (
+)
 
-# plot(
-# 	s1m,
-# 	title = "Plus-Minus Sequence up to $(length(s1m)) values",
-# 	xlabel = "Sequence index",
-# 	ylabel = "Sequence value",
-# 	legend = false,
-# )
+s = generate_plus_minus_sequence(2120)
 
-# Calculate differences between consecutive entries
-differences = diff(s1m)
-
-
-# # Find the most common difference value
-# most_common_diff = mode(differences)
-# println("Most common difference value: ", most_common_diff)
-
-
-
-
-# Plot the differences
-plot(
-	differences,
-	title = "Differences in Plus-Minus Sequence up to $(length(s1m))",
-	xlabel = "Index",
-	ylabel = "Difference",
+plot1 = scatter(
+	s,
+	marker = :circle,
+	title = "First 100 values",
+	xlabel = "Sequence index",
+	ylabel = "Sequence value",
+	labelfontsize = 8,
 	legend = false,
+	titlefont = (sub_title_size, "Helvetica", :blue),
+)
+
+s = generate_plus_minus_sequence(100000)
+
+plot2 = plot(
+	s,
+	title = "First $(length(s)) values",
+	xlabel = "Sequence index",
+	ylabel = "Sequence value",
+	labelfontsize = 8,
+	legend = false,
+	titlefont = (sub_title_size, "Helvetica", :blue),
+)
+
+big_n = 1_000_000
+s = generate_plus_minus_sequence(big_n)
+
+plot3 = plot(
+	s,
+	title = "First $(length(s)) values; slope ≈ $((s[end] - s[1]) / big_n)",
+	xlabel = "Sequence index",
+	ylabel = "Sequence value",
+	labelfontsize = 8,
+	legend = false,
+	titlefont = (sub_title_size, "Helvetica", :blue),
+)
+
+differences = [s[i+1] - s[i] for i in 1:length(s)-1]
+
+plot4 = plot(differences,
+	title = "First $(length(differences)) differences",
+	xlabel = "nth difference",
+	ylabel = "Difference",
+	labelfontsize = 8,
+	legend = false,
+	titlefont = (sub_title_size, "Helvetica", :blue),
+)
+
+plot(plot1, plot2, plot3, plot4,
+	layout = layout,
+	size = (plot_size_x, plot_size_y * 4),
+	plot_title = "Plus-Minus Sequence",
+	titlefont = (main_title_size, "Helvetica", :blue),
 )
