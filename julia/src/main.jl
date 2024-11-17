bf3a9cbd968a94813802d94182598d74ee75429c
RationalMath <samerweb3.@gmail.com>
2024-11-17 10:38:14 +0100

    plots wip

--- julia/src/main.jl
+++ julia/src/main.jl
@@ -1,7 +1,8 @@
-using Plots: px
 include("PlusMinusSequence.jl")
-using .PlusMinusSequence
+using .PlusMinusSequence: generate_plus_minus_sequence
 using Statistics
+using Plots
+using Plots: px	
 
 num_plots = 5
 plot_size_x = 600
@@ -13,13 +14,10 @@ sub_title_size = 8
 # Create plot layout with 1 row and 2 columns
 layout = @layout [a; b; c; d; e]
 
-plot_settings = (
-)
-
-s = generate_plus_minus_sequence(2120)
+s100 = generate_plus_minus_sequence(2120)
 
 plot1 = scatter(
-	s,
+	s100,
 	marker = :circle,
 	title = "First 100 values",
 	xlabel = "Sequence index",
@@ -29,11 +27,11 @@ plot1 = scatter(
 	titlefont = (sub_title_size, "Helvetica", :blue),
 )
 
-s = generate_plus_minus_sequence(100000)
+s0 = generate_plus_minus_sequence(100000)
 
 plot2 = plot(
-	s,
-	title = "First $(length(s)) values",
+	s0,
+	title = "First $(length(s0)) values",
 	xlabel = "Sequence index",
 	ylabel = "Sequence value",
 	labelfontsize = 8,
@@ -42,15 +40,15 @@ plot2 = plot(
 )
 
 big_n = 1_000_000
-s = generate_plus_minus_sequence(big_n)
+s1 = generate_plus_minus_sequence(big_n)
 
 padding = 1
 
-slope_avg = mean([s[i+1] - s[i] for i in padding:length(s)-1])
+slope_avg = mean([s1[i+1] - s1[i] for i in padding:length(s1)-1])
 
 plot3 = plot(
-	s,
-	title = "First $(length(s)) values; slope ≈ $(slope_avg)",
+	s1,
+	title = "First $(length(s1)) values; avg slope ≈ $(round(slope_avg, digits=2))",
 	xlabel = "Sequence index",
 	ylabel = "Sequence value",
 	labelfontsize = 8,
@@ -60,12 +58,26 @@ plot3 = plot(
 	label = "Plus-Minus Sequence",
 )
 
-line_data = [slope_avg * i for i in 1:length(s)]
+line_data = [slope_avg * i for i in 1:length(s1)]
 plot!(line_data, label = "y = $(round(slope_avg, digits = 2))x", color = :red)
 
-differences = [s[i+1] - s[i] for i in 1:length(s)-1]
+s3 = generate_plus_minus_sequence(10000)
+differences = [s3[i+1] - s3[i] for i in 1:length(s3)-1]
 
-plot4 = plot(differences,
+window_size = 1000
+diff_moving_avg = [mean(differences[i:i+window_size]) for i in 1:length(differences)-window_size]
+
+plot4 = plot(diff_moving_avg,
+	title = "Moving average over first $(length(diff_moving_avg)) differences (window size = $(window_size))",
+	xlabel = "nth difference",
+	ylabel = "Difference",
+	labelfontsize = 8,
+	titlefont = (sub_title_size, "Helvetica", :blue),
+)
+
+
+
+plot5 = plot(differences,
 	title = "First $(length(differences)) differences",
 	xlabel = "nth difference",
 	ylabel = "Difference",
@@ -76,17 +88,6 @@ plot4 = plot(differences,
 
 constant_line = [12 for i in 1:length(differences)]
 plot!(constant_line, label = "y = 12", color = :red, opacity = 0.5, linewidth = 3)
-
-window_size = 1000
-diff_moving_avg = [mean(differences[i:i+window_size]) for i in 1:length(differences)-window_size]
-
-plot5 = plot(diff_moving_avg,
-	title = "Moving average over first $(length(diff_moving_avg)) differences (window size = $(window_size))",
-	xlabel = "nth difference",
-	ylabel = "Difference",
-	labelfontsize = 8,
-	titlefont = (sub_title_size, "Helvetica", :blue),
-)
 
 plot(plot1, plot2, plot3, plot4, plot5,
 	layout = layout,
